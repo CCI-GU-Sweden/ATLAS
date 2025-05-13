09658e65c4fed8323dbab593958d5b30f38a2dca
Anders Folkesson <anders.folkesson@gu.se>
2025-05-13 12:28:24 +0200

    added a setup.py file to make this into a pip installable module

---
+++ atlas/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atlas",
+    version="0.1.0",
+    description="Your package description here",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/your-username/ATLAS",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        # List your dependencies here, e.g.:
+        # "numpy>=1.19.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
